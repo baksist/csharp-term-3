ee7e4729fe24f92cb5ea18c1e128b05379cdf90f
matt <matvey.serdukov@gmail.com>
2020-09-17 16:28:42 +0300

    task 2.1.2 completed

---
+++ pr-02/pr-02-01/pr-02-01-02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace pr_02_01_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //так как .ini-файлы - фишка винды, для разнообразия будем мониторить логи! 
+            FileSystemWatcher watcher = new FileSystemWatcher("/var/log");
+            //watcher.Filter = "*.ini";
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.Size;
+            watcher.Changed += new FileSystemEventHandler(watcher_Changed);
+            watcher.EnableRaisingEvents = true;
+            Console.Read();
+        }
+        static void watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Changed: {0}", e.FullPath);
+        }
+    }
+}
