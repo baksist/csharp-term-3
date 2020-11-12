a2229648d5d70c023cb6f92f46b000bc6a4479e7
matt <matvey.serdukov@gmail.com>
2020-11-12 18:49:50 +0300

    completed task 11.2

---
+++ pr-11/pr-11-02/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace pr_11_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.grayButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 29);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(100, 23);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // grayButton
+            // 
+            this.grayButton.Location = new System.Drawing.Point(428, 29);
+            this.grayButton.Name = "grayButton";
+            this.grayButton.Size = new System.Drawing.Size(100, 23);
+            this.grayButton.TabIndex = 3;
+            this.grayButton.Text = "B/W";
+            this.grayButton.UseVisualStyleBackColor = true;
+            this.grayButton.Click += new System.EventHandler(this.grayButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(857, 29);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(557, 104);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 516);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.grayButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button grayButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ pr-11/pr-11-02/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pr_11_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image files(*.bmp, *.jpg, *.gif, *.png)|*.bmp;*.jpg;*.gif;*.png|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Can't open file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                var sfd = new SaveFileDialog();
+                sfd.Title = "Save image as...";
+
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true;
+
+                sfd.Filter = "Image files(*.bmp)|*.bmp|Image files(*.jpg)|*.jpg|Image files(*.gif)|*.gif|Image files(*.png)|*.png)|All files (*.*)|*.*";
+                sfd.ShowHelp = true;
+                
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox2.Image.Save(sfd.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Can't save file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void grayButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                var input = new Bitmap(pictureBox1.Image);
+                var output = new Bitmap(input.Width, input.Height);
+
+                for (int j = 0; j < input.Height; j++)
+                {
+                    for (int i = 0; i < input.Width; i++)
+                    {
+                        var pixel = (UInt32)(input.GetPixel(i, j).ToArgb());
+                        var r = (float)((pixel & 0x00FF0000) >> 16);
+                        var g = (float)((pixel & 0x0000FF00) >> 8);
+                        var b = (float)(pixel & 0x000000FF);
+                        r = g = b = (r + g + b) / 3.0f;
+
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)r << 16) | ((UInt32)g << 8) | (UInt32)b;
+                        output.SetPixel(i, j, Color.FromArgb((int)newPixel));
+                    }
+                }
+                pictureBox2.Image = output;
+            }
+        }
+    }
+}
